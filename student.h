018db0cd41f847d62b0aa1d52979cadfc853e590
nkdsilva <77807225+nkdsilva@users.noreply.github.com>
2022-11-21 13:35:56 +0530

    Student header file

---
+++ student.h
@@ -0,0 +1,13 @@
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+typedef struct student_marks
+{
+  char student_index[12];
+  float assgnmt01_marks;
+  float assgnmt02_marks;
+  float project_marks;
+  float finalExam_marks;
+}student_marks;
+
+#endif
